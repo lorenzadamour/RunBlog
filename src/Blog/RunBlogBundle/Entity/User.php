0b0ca9ae009fe8fa73ef6cda88eb6066ef2f0435
Sweezy974 <sweezy974@live.fr>
2016-07-26 14:04:40 +0400

    ajout de champs inscription

--- src/Blog/RunBlogBundle/Entity/User.php
+++ src/Blog/RunBlogBundle/Entity/User.php
@@ -78,13 +78,36 @@ class User extends BaseUser
     */
     private $avis;
 
+    /**
+    * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+    * of 'UploadedFile' is injected into this setter to trigger the  update. If this
+    * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+    * must be able to accept an instance of 'File' as the bundle will inject one here
+    * during Doctrine hydration.
+    *
+    * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $image
+    *
+    * @return Product
+    */
+        public function setImageFile(File $image = null)
+        {
+            $this->imageFile = $image;
 
-    public function __construct()
-    {
-        parent::__construct();
-        // your own logic
+              if ($image) {
+        // It is required that at least one field changes if you are using doctrine
+        // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTime('now');
+      }
+            return $this;
     }
+    /**
+    * @return File
+    */
+        public function getImageFile()
+        {
 
+      return $this->imageFile;
+        }
 
 
     /**
@@ -160,6 +183,54 @@ class User extends BaseUser
     }
 
     /**
+     * Set imageName
+     *
+     * @param string $imageName
+     *
+     * @return User
+     */
+    public function setImageName($imageName)
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    /**
+     * Get imageName
+     *
+     * @return string
+     */
+    public function getImageName()
+    {
+        return $this->imageName;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return User
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
      * Add commentaire
      *
      * @param \Blog\RunBlogBundle\Entity\Commentaire $commentaire
@@ -225,77 +296,5 @@ class User extends BaseUser
     public function getAvis()
     {
         return $this->avis;
-    }
-
-    /**
-     * Set image
-     *
-     * @param \Blog\RunBlogBundle\Entity\Image $image
-     *
-     * @return User
-     */
-    public function setImage(\Blog\RunBlogBundle\Entity\Image $image = null)
-    {
-        $this->image = $image;
-
-        return $this;
-    }
-
-    /**
-     * Get image
-     *
-     * @return \Blog\RunBlogBundle\Entity\Image
-     */
-    public function getImage()
-    {
-        return $this->image;
-    }
-
-    /**
-     * Set imageName
-     *
-     * @param string $imageName
-     *
-     * @return User
-     */
-    public function setImageName($imageName)
-    {
-        $this->imageName = $imageName;
-
-        return $this;
-    }
-
-    /**
-     * Get imageName
-     *
-     * @return string
-     */
-    public function getImageName()
-    {
-        return $this->imageName;
-    }
-
-    /**
-     * Set updatedAt
-     *
-     * @param \DateTime $updatedAt
-     *
-     * @return User
-     */
-    public function setUpdatedAt($updatedAt)
-    {
-        $this->updatedAt = $updatedAt;
-
-        return $this;
-    }
-
-    /**
-     * Get updatedAt
-     *
-     * @return \DateTime
-     */
-    public function getUpdatedAt()
-    {
-        return $this->updatedAt;
     }
 }
---
+++ src/Blog/RunBlogBundle/Form/UserType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Blog\RunBlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('dateDeNaissance', DateType::class, array('years' => range(1920,1998),))
+            ->add('imageFile', VichImageType::class, array('label' => ' ', 'required' => true))
+
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Blog\RunBlogBundle\Entity\User'
+        ));
+    }
+    public function getParent()
+  {
+    return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+  }
+}
